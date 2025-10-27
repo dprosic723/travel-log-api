3a570ad1552f0d4b21783262468525b36a06f074
David Prosic <dprosic723@gmail.com>
2025-10-27 11:38:54 +0100

    update TravelLog model fields

--- app/Models/TravelLog.php
+++ app/Models/TravelLog.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TravelLog extends Model
 {
-    //
+        use HasFactory, HasUuids;
+        public $incrementing = false;
+        public $keytype = 'string';
+
+        protected $fillable = [
+            'type',
+            'departure_date',
+            'arrival_date',
+            'departure_place',
+            'arrival_place',
+            'accommodation_place',
+            'comment'
+        ];
 }
